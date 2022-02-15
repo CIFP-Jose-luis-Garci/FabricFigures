ca8ddda688ea105da35fe087eb7643570f377e13
BlancaSJG <blancasanjose13@gmail.com>
2022-02-15 13:46:42 +0100

    Sliders audio
    Los sliders están conectados al Audio Mixer, pero no recuerdan los valores al volver a entrar en la escena.
    Lo que se necesita es que al cambiar los valores, entrar a otra escena en Play y volver a la de Audio, los valores permanezcan iguales.
    De igual forma, tanto el valor escrito (provisional) y la animación del slider también debería cuadrar al volver a la escena.

---
+++ Unity/FabricFigures v.0.1/Assets/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //audio
+    public static float musicVolumen;
+    public static float sfxVolumen;
+    public static float volumeVolumen;
+}
--- Unity/FabricFigures v.0.1/Assets/Scenes/Menus/ScriptsMenus/MenuScript.cs
+++ Unity/FabricFigures v.0.1/Assets/Scenes/Menus/ScriptsMenus/MenuScript.cs
@@ -2,9 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuScript : MonoBehaviour
 {
+    //Audio
+
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    [SerializeField] AudioMixer masterMixer;
+
+    //Text
+
+    [SerializeField] Text volumeText;
+    [SerializeField] Text musicText;
+    [SerializeField] Text sfxText;
+
+
+
+    private void Start()
+    {
+        sfxSlider.value = GameManager.sfxVolumen;
+        musicSlider.value = GameManager.musicVolumen;
+        volumeSlider.value = GameManager.volumeVolumen;
+
+        musicText.text = musicSlider.value.ToString();
+        sfxText.text = sfxSlider.value.ToString();
+        volumeText.text = volumeSlider.value.ToString();
+
+    }
+
+    private void Update()
+    {
+        HudText();
+    }
+
+
+    public void SetSfxLvl(float sfxVol)
+    {
+
+        masterMixer.SetFloat("sfxVol", sfxVol);
+        PlayerPrefs.SetFloat("sfxVol", sfxVol);
+        GameManager.sfxVolumen = PlayerPrefs.GetFloat("sfxVol");
+
+    }
+
+    public void SetMusicLvl(float musicVol)
+    {
+        masterMixer.SetFloat("musicVol", musicVol);
+
+        PlayerPrefs.SetFloat("musicVol", musicVol);
+        GameManager.musicVolumen = PlayerPrefs.GetFloat("musicVol");
+
+
+    }
+
+    public void SetVolumeLvl(float volVol)
+    {
+        masterMixer.SetFloat("volVol", volVol);
+
+        PlayerPrefs.SetFloat("volVol", volVol);
+        GameManager.volumeVolumen = PlayerPrefs.GetFloat("volVol");
+
+
+    }
+
+
+    public void HudText()
+    {
+        
+        musicText.text = musicSlider.value.ToString();
+        sfxText.text = sfxSlider.value.ToString();
+        volumeText.text = volumeSlider.value.ToString();
+
+
+
+    }
+
+
+
+
+
+
     public void CargarEscena(int escena)
     {
         SceneManager.LoadScene(escena);
