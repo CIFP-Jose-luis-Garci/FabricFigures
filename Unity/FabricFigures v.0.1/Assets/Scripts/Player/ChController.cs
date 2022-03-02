ed1c3bbf5cd92b9c27f6e05db660b7e363c3a6d5
Maya Pardo <mayasantayana@hotmail.com>
2022-03-02 13:19:08 +0100

    Idle, jumping and falling animations

--- Unity/FabricFigures v.0.1/Assets/Scripts/Player/ChController.cs
+++ Unity/FabricFigures v.0.1/Assets/Scripts/Player/ChController.cs
@@ -238,6 +238,8 @@ public class ChController : MonoBehaviour
 
     void Update()
     {
+        animator.SetBool("IsGrounded", controller.isGrounded);
+
         if ((isCharging || isAttacking) && controller.isGrounded)
         {
             speed = 0f;
@@ -261,6 +263,7 @@ public class ChController : MonoBehaviour
         if(isAim && aimTarget != null)
             FocusParticles();
 
+        
     }
 
 
@@ -349,10 +352,14 @@ public class ChController : MonoBehaviour
         }
         moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-      
 
-        velocity.y -= gravity * Time.deltaTime;
+        if (controller.isGrounded)
+            velocity.y = -1 * Time.deltaTime;
+        else
+            velocity.y -= gravity * Time.deltaTime;
+
         controller.Move(new Vector3(moveDir.x * speed * moveMult, velocity.y, moveDir.z * speed * moveMult) * Time.deltaTime);
+        animator.SetFloat("Y vel", velocity.y);
     }
 
 
